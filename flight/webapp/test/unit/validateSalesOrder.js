67cddb46fb65bfb92e855f4e6093b862f5bb73f9
ziatahir <46965613+ziatahir@users.noreply.github.com>
2020-03-03 15:54:31 +0530

    Update validateSalesOrder.js

--- flight/webapp/test/unit/validateSalesOrder.js
+++ flight/webapp/test/unit/validateSalesOrder.js
@@ -8,24 +8,9 @@ function validateSalesDocNumber(SalOrdNumber) {
         }
         return status;
 }
-onValidate: function(oEvent) {
-			if (!this.validateSalesDocNumber(oEvent.getSource().getValue())) {
-				oEvent.getSource().setValueState("Error");
-				MessageBox.error("Personnel number should be minimum 10 digits.");
-			} else {
-				oEvent.getSource().setValueState("Success");
-			}
-}
+
 
 function addTwoNumbers(NumberOne, NumberTwo) {
     "use strict";
      return numberOne + numberTwo;
-}
-onValidate: function(oEvent) {
-			if (!this.addTwoNumbers(oEvent.getSource().getValue())) {
-				oEvent.getSource().setValueState("Error");
-				MessageBox.error("Personnel number should be minimum 10 digits.");
-			} else {
-				oEvent.getSource().setValueState("Success");
-			}
 }
