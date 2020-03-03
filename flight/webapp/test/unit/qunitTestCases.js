9093addb5eaba86103f4ee12513a4a6f82209dc7
ziatahir <46965613+ziatahir@users.noreply.github.com>
2020-03-03 14:42:53 +0530

    Update qunitTestCases.js

--- flight/webapp/test/unit/qunitTestCases.js
+++ flight/webapp/test/unit/qunitTestCases.js
@@ -1,11 +1,14 @@
 QUnit.test("Invalid Sales Document Number", function(assert) {
+      "use strict";
       assert.equal( true, validateSalesDocNumber(1234567890), "length is fine" );
 });
 
 QUnit.test("Invalid Sales Document Number", function(assert) {
+      "use strict";
       assert.equal( false, validateSalesDocNumber(134567890), "length is fine" );
 });
 
 QUnit.test( "addTwoNumbers test", function( assert ) {
+    "use strict";
     assert.equal( 5, addTwoNumbers( 3, 2 ), "5, addTwoNumbers(3, 2); equal succeeds" );
 });
