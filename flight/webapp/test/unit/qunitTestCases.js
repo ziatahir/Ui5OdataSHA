6a37018be010f7852c4dee1385123f5e3ceb494e
ziatahir <46965613+ziatahir@users.noreply.github.com>
2020-03-03 15:54:56 +0530

    Update qunitTestCases.js

--- flight/webapp/test/unit/qunitTestCases.js
+++ flight/webapp/test/unit/qunitTestCases.js
@@ -12,3 +12,22 @@ QUnit.test( "addTwoNumbers test", function( assert ) {
     "use strict";
     assert.equal( 5, addTwoNumbers( 3, 2 ), "5, addTwoNumbers(3, 2); equal succeeds" );
 });
+
+
+
+function validateSalesDocNumber(SalOrdNumber) {
+       "use strict";
+       var status;
+       if (salOrdNumber.toString().length > 9) {
+            status = true;
+        } else {
+            status = false;
+        }
+        return status;
+}
+
+
+function addTwoNumbers(NumberOne, NumberTwo) {
+    "use strict";
+     return numberOne + numberTwo;
+}
